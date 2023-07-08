2b70984bfbd1cd918f7e04c7fefd312fb06b7330
NightWolf Software Development <nightwolfdevelopment@gmail.com>
2023-07-07 21:25:14 -0400

    Check for github route in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent { 
+
+  constructor(private router: Router) {
+    const route = localStorage.getItem('route');
+
+    if (route) {
+      localStorage.removeItem('route');
+      this.router.navigate([route]);
+    }
+  }
+
+}
